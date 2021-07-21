3edb56cb26ea5633da63fc706fb238b884cf63b2
Karlatemp <karlatemp@vip.qq.com>
2021-07-21 20:45:06 +0800

    Fix sql drivers loading

--- src/main/java/io/github/kasukusakura/mcsp/MiraiConsoleSystemProvider.java
+++ src/main/java/io/github/kasukusakura/mcsp/MiraiConsoleSystemProvider.java
@@ -2,6 +2,11 @@ package io.github.kasukusakura.mcsp;
 
 import net.mamoe.mirai.console.plugin.jvm.JavaPlugin;
 import net.mamoe.mirai.console.plugin.jvm.JvmPluginDescriptionBuilder;
+import net.mamoe.mirai.utils.MiraiLogger;
+
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.util.Enumeration;
 
 public class MiraiConsoleSystemProvider extends JavaPlugin {
     public static final MiraiConsoleSystemProvider INSTANCE = new MiraiConsoleSystemProvider();
@@ -17,6 +22,28 @@ public class MiraiConsoleSystemProvider extends JavaPlugin {
 
     @Override
     public void onEnable() {
+        MiraiLogger logger = getLogger();
         getLogger().info("Loaded");
+        {
+            Enumeration<Driver> drivers = DriverManager.getDrivers();
+            logger.info("Loaded sql drivers:");
+            while (drivers.hasMoreElements()) {
+                logger.info(" `- " + drivers.nextElement());
+            }
+        }
+    }
+
+    static {
+        try {
+            for (String sqlC : new String[]{
+                    "org.h2.Driver",
+                    "com.mysql.cj.jdbc.Driver",
+                    "org.postgresql.Driver",
+            }) {
+                Class.forName(sqlC);
+            }
+        } catch (Throwable throwable) {
+            throw new ExceptionInInitializerError(throwable);
+        }
     }
 }
